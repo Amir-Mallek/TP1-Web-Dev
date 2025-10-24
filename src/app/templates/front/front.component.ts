5b9cc0d69ae0d5c6dc289d7ae3a9ecd2dc5e14a8
Moncef Koubaa <moncefkoubaa26@gmail.com>
2025-10-24 10:26:07 +0100

    feat : TTC Calculator using signals

--- src/app/templates/front/front.component.ts
+++ src/app/templates/front/front.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TTCComponent } from 'src/app/ttc/ttc.component';
 
 @Component({
-    selector: 'app-front',
-    templateUrl: './front.component.html',
-    styleUrls: ['./front.component.css'],
-    standalone: true,
-    imports: [RouterOutlet]
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.css'],
+  standalone: true,
+  imports: [RouterOutlet, TTCComponent],
 })
-export class FrontComponent {
-
-}
+export class FrontComponent {}
---
+++ src/app/ttc/ttc.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-ttc',
+  standalone: true,
+  imports: [DecimalPipe],
+  templateUrl: './ttc.component.html',
+  styleUrl: './ttc.component.css',
+})
+export class TTCComponent {
+  PriceHT: WritableSignal<number> = signal(0);
+  TVA: WritableSignal<number> = signal(0);
+  Quantity: WritableSignal<number> = signal(0);
+  PriceUnitTTC: Signal<number> = computed(
+    () => this.PriceHT() * (1 + this.TVA() / 100)
+  );
+  TotalTTC: Signal<number> = computed(() =>
+    parseFloat((this.PriceUnitTTC() * this.Quantity()).toFixed(2))
+  );
+}
