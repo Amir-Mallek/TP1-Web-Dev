148aec335233eef859f0d8f619a957703ce76357
Moncef Koubaa <moncefkoubaa26@gmail.com>
2025-10-24 18:34:11 +0100

    feat : apply signals on cvTech

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,49 +1,53 @@
-import { Component, inject } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
-import { ListComponent } from "../list/list.component";
-import { CvCardComponent } from "../cv-card/cv-card.component";
-import { EmbaucheComponent } from "../embauche/embauche.component";
-import { UpperCasePipe, DatePipe } from "@angular/common";
+import { Component, computed, inject } from '@angular/core';
+import { Cv } from '../model/cv';
+import { LoggerService } from '../../services/logger.service';
+import { ToastrService } from 'ngx-toastr';
+import { CvService } from '../services/cv.service';
+import { ListComponent } from '../list/list.component';
+import { CvCardComponent } from '../cv-card/cv-card.component';
+import { EmbaucheComponent } from '../embauche/embauche.component';
+import { UpperCasePipe, DatePipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
 @Component({
-    selector: "app-cv",
-    templateUrl: "./cv.component.html",
-    styleUrls: ["./cv.component.css"],
-    standalone: true,
-    imports: [
-        ListComponent,
-        CvCardComponent,
-        EmbaucheComponent,
-        UpperCasePipe,
-        DatePipe,
-    ],
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.css'],
+  standalone: true,
+  imports: [
+    ListComponent,
+    CvCardComponent,
+    EmbaucheComponent,
+    UpperCasePipe,
+    DatePipe,
+  ],
 })
 export class CvComponent {
   private logger = inject(LoggerService);
   private toastr = inject(ToastrService);
   private cvService = inject(CvService);
-
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
+  // why this is better than subscribe + BehaviorSubject?
+  // 1. automatic unsubscription
+  // 2. better integration with Angular's change detection
+  // the signal updates only the parts of the template that depend on it
+  // while BehaviorSubject triggers a full change detection cycle
+  cvs = toSignal(
+    this.cvService.getCvs().pipe(
+      catchError((err) => {
+        this.toastr.error(`
+          Attention!! Les données sont fictives, problème avec le serveur.
+          Veuillez contacter l'admin.`);
+        return of(this.cvService.getFakeCvs());
+      })
+    ),
+    { initialValue: [] }
+  );
+  selectedCv = computed(() => this.cvService.selectedCv());
   /*   selectedCv: Cv | null = null; */
   date = new Date();
 
   constructor() {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
-    });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+    this.logger.logger('je suis le cvComponent');
+    this.toastr.info('Bienvenu dans notre CvTech');
   }
 }
